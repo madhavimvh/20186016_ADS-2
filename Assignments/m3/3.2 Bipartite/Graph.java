12040a117ac74e421fa806b24306e2e6e4f0a1c6
madhavimvh <mvhmadhavi@msitprogram.net>
2018-10-31 22:11:00 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m3/3.2 Bipartite/Graph.java
+++ Assignments/m3/3.2 Bipartite/Graph.java
@@ -1,38 +1,3 @@
-/*************************************************************************
- *  Compilation:  javac Graph.java        
- *  Execution:    java Graph input.txt
- *  Dependencies: Bag.java In.java StdOut.java
- *  Data files:   http://algs4.cs.princeton.edu/41undirected/tinyG.txt
- *
- *  A graph, implemented using an array of sets.
- *  Parallel edges and self-loops allowed.
- *
- *  % java Graph tinyG.txt
- *  13 vertices, 13 edges 
- *  0: 6 2 1 5 
- *  1: 0 
- *  2: 0 
- *  3: 5 4 
- *  4: 5 6 3 
- *  5: 3 4 0 
- *  6: 0 4 
- *  7: 8 
- *  8: 7 
- *  9: 11 10 12 
- *  10: 9 
- *  11: 9 12 
- *  12: 11 9 
- *
- *  % java Graph mediumG.txt
- *  250 vertices, 1273 edges 
- *  0: 225 222 211 209 204 202 191 176 163 160 149 114 97 80 68 59 58 49 44 24 15 
- *  1: 220 203 200 194 189 164 150 130 107 72 
- *  2: 141 110 108 86 79 51 42 18 14 
- *  ...
- *  
- *************************************************************************/
-
-
 /**
  * The <tt>Graph</tt> class represents an undirected graph of vertices named 0
  * through V-1. It supports the following operations: add an edge to the graph,
@@ -57,15 +22,17 @@ public class Graph {
      * { var_description }.
      */
     private Bag<Integer>[] adj;
-    
    /**
      * Create an empty graph with V vertices.
      *
      * @param      ver   The version
      */
-    public Graph(final int ver) {
-        if (ver < 0) throw new RuntimeException("Number of vertices must be nonnegative");
-        this.ver = ver;
+    public Graph(final int verr) {
+        if (verr < 0) {
+            throw new RuntimeException(
+            "Number of vertices must be nonnegative");
+        }
+        this.ver = verr;
         this.edg = 0;
         adj = (Bag<Integer>[]) new Bag[ver];
         for (int v = 0; v < ver; v++) {
@@ -80,19 +47,19 @@ public class Graph {
      * @param      ver   The version
      * @param      edg   The edg
      */
-    public Graph(final int ver, final int edg) {
-        this(ver);
+    public Graph(final int verr, final int edgg) {
+        this(verr);
         if (edg < 0) {
             throw new RuntimeException("Number of edges must be nonnegative");
         }
         for (int i = 0; i < edg; i++) {
-            int v = (int) (Math.random() * ver);
-            int w = (int) (Math.random() * ver);
+            int v = (int) (Math.random() * verr);
+            int w = (int) (Math.random() * verr);
             addEdge(v, w);
         }
     }
 
-   /**  
+   /**
      * Create a digraph from input stream.
      */
 /*    public Graph(In in) {
