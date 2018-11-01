118f153eb86a591331bae512adc1d0519c8d865b
madhavimvh <mvhmadhavi@msitprogram.net>
2018-11-01 21:03:23 +0530

    "ads2_m4_a1_1: 5/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ Assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -6,13 +6,18 @@ public class Solution {
 		String type = StdIn.readString();
 		try {
 		if (type.equals("Graph")) {
-		WordNet wordnet = new WordNet(synsets, hypernyms);
+			WordNet wordnet = new WordNet(synsets, hypernyms);
 		//wordnet.readhyn()
 		} 
-	} catch (Exception ex) {
-		System.out.println(ex.getMessage());
-		// else if (type.equals("Queries"))
-	}
-
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		if (type.equals("Queries")) {
+			String[] arr = StdIn.readString().split(" ");
+			if (arr[0] == null) {
+				System.out.println("IllegalArgumentException");
+			}
+			
+		}
 	}
 }
