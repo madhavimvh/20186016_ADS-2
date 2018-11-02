35570b3dd232e74822e7563900b2bf4665761488
madhavimvh <mvhmadhavi@msitprogram.net>
2018-11-02 18:02:07 +0530

    "ads2_m4_a1_1: 7/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m4/Code Camp - WordNet/WordNet/SAP.java
+++ Assignments/m4/Code Camp - WordNet/WordNet/SAP.java
@@ -1,19 +1,12 @@
 public class SAP {
     private Digraph gr;
+    private int ancestor;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph gr) {
         this.gr = gr;
+        ancestor = -1;
     }
-
-    // length of shortest ancestral path between v and w; -1
-    // if no such path
-    // public int length(int v, int w) {
-    // }
-
-    // a common ancestor of v and w that participates in a
-    // shortest ancestral path; -1 if no such path
-    // public int ancestor(int v, int w)
 
     // length of shortest ancestral path between any vertex in v
     // and any vertex in w; -1 if no such path
@@ -25,6 +18,7 @@ public class SAP {
             if (bfdv.hasPathTo(i) && bfdw.hasPathTo(i)) {
                 int dist = bfdv.distTo(i) + bfdw.distTo(i);
                 if (dist < min) {
+                    ancestor = i;
                     min = dist;
                 }
                 
@@ -36,9 +30,8 @@ public class SAP {
     // a common ancestor that participates in shortest
     // ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        int min = length(v, w);
-        return min;
-        
+        length(v, w);
+        return ancestor;
     }
 
     // do unit testing of this class
