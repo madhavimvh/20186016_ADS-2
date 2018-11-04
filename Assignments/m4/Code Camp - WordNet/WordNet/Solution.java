97354d86bbc972f32b2763aa8ccc6c280b5d4fc4
madhavimvh <mvhmadhavi@msitprogram.net>
2018-11-05 00:56:59 +0530

    "ads2_m4_a1_1: 0/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ Assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -1,38 +1,39 @@
-import java.util.Arrays;
+// import java.util.Arrays;
 /**
  * { item_description }.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		String synsets = StdIn.readString();
-		String hypernyms = StdIn.readString();
-		String type = StdIn.readString();
-		try {
-			WordNet wordnet = new WordNet(synsets, hypernyms);
-			if (type.equals("Graph")) {
-				System.out.println(wordnet.getDigraph());
-			} else {
-				while (!StdIn.isEmpty()) {
-					String nounA = StdIn.readString();
-					String nounB = StdIn.readString();
-					System.out.println("distance = " +
-					                   wordnet.distance(nounA, nounB) + ", ancestor = " +
-					                   wordnet.sap(nounA, nounB));
-				}
-			}
-		} catch (Exception ex) {
-			//ex.printStackTrace();
-			System.out.println(ex.getMessage());
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        String synsets = StdIn.readString();
+        String hypernyms = StdIn.readString();
+        String type = StdIn.readString();
+        try {
+            WordNet wordnet = new WordNet(synsets, hypernyms);
+            if (type.equals("Graph")) {
+                System.out.println(wordnet.getDigraph());
+            } else {
+                while (!StdIn.isEmpty()) {
+                    String nounA = StdIn.readString();
+                    String nounB = StdIn.readString();
+                    System.out.println("distance = "
+                        + wordnet.distance(nounA, nounB)
+                        + ", ancestor = "
+                        + wordnet.sap(nounA, nounB));
+                }
+            }
+        } catch (Exception ex) {
+            //ex.printStackTrace();
+            System.out.println(ex.getMessage());
+        }
+    }
 }
