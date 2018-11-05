eb8600dd59cac8cffb7e830eaf96a7a0f989d0d5
madhavimvh <mvhmadhavi@msitprogram.net>
2018-11-05 15:51:51 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m7/1. MST/Solution.java
+++ Assignments/m7/1. MST/Solution.java
@@ -3,31 +3,31 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int ver = Integer.parseInt(scan.nextLine());
-		int edg = Integer.parseInt(scan.nextLine());
-		EdgeWeightedGraph edgGraph = new EdgeWeightedGraph(ver);
-		while (scan.hasNext()) {
-		String[] arr = scan.nextLine().split(" ");
-		Edge edge = new Edge(Integer.parseInt(arr[0]),
-			Integer.parseInt(arr[1]), Double.parseDouble(arr[2]));
-		edgGraph.addEdge(edge);
-	}
-	LazyPrimMST prim = new LazyPrimMST(edgGraph);
-	System.out.printf("%.5f\n", prim.weight());
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int ver = Integer.parseInt(scan.nextLine());
+        int edg = Integer.parseInt(scan.nextLine());
+        EdgeWeightedGraph edgGraph = new EdgeWeightedGraph(ver);
+        while (scan.hasNext()) {
+        String[] arr = scan.nextLine().split(" ");
+        Edge edge = new Edge(Integer.parseInt(arr[0]),
+            Integer.parseInt(arr[1]), Double.parseDouble(arr[2]));
+        edgGraph.addEdge(edge);
+    }
+    LazyPrimMST prim = new LazyPrimMST(edgGraph);
+    System.out.printf("%.5f\n", prim.weight());
 
-	}
+    }
 }
 
 
