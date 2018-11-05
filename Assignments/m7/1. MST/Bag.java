927305f87edb258d4964691d2afe788d4f461c27
madhavimvh <mvhmadhavi@msitprogram.net>
2018-11-05 16:02:03 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m7/1. MST/Bag.java
+++ Assignments/m7/1. MST/Bag.java
@@ -9,12 +9,11 @@ public class Bag<Item> implements Iterable<Item> {
     /**
      * { var_description }.
      */
-    private Node<Item> first;    
+    private Node<Item> first;
     /**
      * { var_description }.
      */
-    private int n;               
-
+    private int n;
     /**
      * Class for node.
      *
@@ -30,7 +29,6 @@ public class Bag<Item> implements Iterable<Item> {
          */
         private Node<Item> next;
     }
-
     /**
      * Initializes an empty bag.
      */
@@ -38,7 +36,6 @@ public class Bag<Item> implements Iterable<Item> {
         first = null;
         n = 0;
     }
-
     /**
      * Returns true if this bag is empty.
      *
@@ -48,7 +45,6 @@ public class Bag<Item> implements Iterable<Item> {
     public boolean isEmpty() {
         return first == null;
     }
-
     /**
      * Returns the number of items in this bag.
      *
@@ -57,7 +53,6 @@ public class Bag<Item> implements Iterable<Item> {
     public int size() {
         return n;
     }
-
     /**
      * Adds the item to this bag.
      *
@@ -70,20 +65,18 @@ public class Bag<Item> implements Iterable<Item> {
         first.next = oldfirst;
         n++;
     }
-
-
     /**
-     * Returns an iterator that iterates over the items in this bag in arbitrary
+     * Returns an iterator that iterates over
+     * the items in this bag in arbitrary
      * order.
      *
-     * @return     an iterator that iterates over the items in this bag in
+     * @return     an iterator that iterates
+     * over the items in this bag in
      *             arbitrary order
      */
     public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);  
+        return new ListIterator<Item>(first);
     }
-
-    
     /**
      * Class for list iterator.
      *
@@ -100,8 +93,8 @@ public class Bag<Item> implements Iterable<Item> {
          *
          * @param      first  The first
          */
-        public ListIterator(Node<Item> first) {
-            current = first;
+        ListIterator(final Node<Item> firstt) {
+            current = firstt;
         }
 
         /**
@@ -113,14 +106,14 @@ public class Bag<Item> implements Iterable<Item> {
             return current != null;
         }
         /**
-         * { function_description }
+         * { function_description }.
          */
         public void remove() {
             throw new UnsupportedOperationException();
         }
 
         /**
-         * { function_description }
+         * { function_description }.
          *
          * @return     { description_of_the_return_value }
          */
@@ -129,7 +122,7 @@ public class Bag<Item> implements Iterable<Item> {
                 throw new NoSuchElementException();
             }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
