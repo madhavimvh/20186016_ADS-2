c78b21766d6ff676696d0f7b8af0c245b1324c62
madhavimvh <mvhmadhavi@msitprogram.net>
2018-11-10 12:33:34 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m12/ADS-2_Exam-2/Solution.java
+++ Assignments/m12/ADS-2_Exam-2/Solution.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.ArrayList;
 public class Solution {
 
 	public static void main(String[] args) {
@@ -48,7 +49,8 @@ public class Solution {
 			String[] ispath1 = scan.nextLine().split(" ");
 			DijkstraUndirectedSP mst1 = new DijkstraUndirectedSP(graph, Integer.parseInt(ispath1[0]));
 			if (mst1.hasPathTo(Integer.parseInt(ispath1[1])) && mst1.hasPathTo(Integer.parseInt(ispath1[2]))) {
-				System.out.println(path(graph, Integer.parseInt(ispath1[0]), Integer.parseInt(ispath1[1]), Integer.parseInt(ispath1[2])));
+				System.out.println(pathdist(graph, Integer.parseInt(ispath1[0]), Integer.parseInt(ispath1[1]), Integer.parseInt(ispath1[2])));
+				path(graph, Integer.parseInt(ispath1[0]), Integer.parseInt(ispath1[1]), Integer.parseInt(ispath1[2]));
 				// System.out.println(mst1.distTo(Integer.parseInt(ispath1[2])));
 			} else {
 				System.out.println("No Path Found.");
@@ -61,15 +63,50 @@ public class Solution {
 		}
 
 	}
-	public static double path(EdgeWeightedGraph graph, int s, int via, int d) {
+	public static double pathdist(EdgeWeightedGraph graph, int s, int via, int d) {
 		DijkstraUndirectedSP mst = new DijkstraUndirectedSP(graph, s);
 		double dist1 = mst.distTo(via);
 		DijkstraUndirectedSP mstt = new DijkstraUndirectedSP(graph, via);
 		double dist2 = mstt.distTo(d);
 		double totaldist = dist1 + dist2;
 		return totaldist;
-
-
-
 	}
+	public static void path(EdgeWeightedGraph graph, int s, int via, int d) {
+		DijkstraUndirectedSP mst = new DijkstraUndirectedSP(graph, s);
+		Iterable<Edge> path1 = mst.pathTo(via);
+		DijkstraUndirectedSP mstt = new DijkstraUndirectedSP(graph, via);
+		Iterable<Edge> path2 = mstt.pathTo(d);
+		ArrayList<Integer> str = new ArrayList<Integer>();
+		str.add(s);
+		for (Edge edge : path1) {
+			int temp = edge.either();
+			if (!str.contains(temp)) {
+				str.add(temp);
+				
+			} else if(!str.contains(edge.other(temp))) {
+				str.add(edge.other(temp));
+			}
+		}
+		for (Edge edge : path2) {
+			int temp1 = edge.either();
+			// System.out.println(edge.either());
+			// System.out.println(edge.other(temp));
+			if (!str.contains(temp1)) {
+				str.add(temp1);
+			} else if (!str.contains(edge.other(temp1))) {
+				str.add(edge.other(temp1));
+			}
+
+		}
+		// System.out.print(path1);
+		// System.out.println(path2);
+		String str1 = "";
+		// System.out.println(str.size());
+		for (int i = 0; i < str.size(); i++) {
+		str1 += str.get(i) + " ";
+		}
+		System.out.println(str1);
+		
+	}
+
 }
