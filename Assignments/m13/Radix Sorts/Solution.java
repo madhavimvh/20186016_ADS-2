cb0ff230e0bd19be9a08b947efe38846260e83d8
madhavimvh <mvhmadhavi@msitprogram.net>
2018-11-12 13:32:30 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m13/Radix Sorts/Solution.java
+++ Assignments/m13/Radix Sorts/Solution.java
@@ -23,21 +23,24 @@ class LSD {
         int R = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
 
-        for (int d = w-1; d >= 0; d--) {
+        for (int d = w - 1; d >= 0; d--) {
             // sort by key-indexed counting on dth character
             // compute frequency counts
-            int[] count = new int[R+1];
+            int[] count = new int[R + 1];
             for (int i = 0; i < n; i++)
                 count[a[i].charAt(d) + 1]++;
             // compute cumulates
-            for (int r = 0; r < R; r++)
-                count[r+1] += count[r];
+            for (int r = 0; r < R; r++) {
+                count[r + 1] += count[r];
+            }
             // move data
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 aux[count[a[i].charAt(d)]++] = a[i];
+            }
             // copy back
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 a[i] = aux[i];
+            }
         }
     }
    /**
@@ -56,34 +59,41 @@ class LSD {
         int[] aux = new int[n];
         for (int d = 0; d < w; d++) {         
             // compute frequency counts
-            int[] count = new int[R+1];
+            int[] count = new int[R + 1];
             for (int i = 0; i < n; i++) {           
-                int c = (a[i] >> BITS_PER_BYTE*d) & MASK;
+                int c = (a[i] >> BITS_PER_BYTE * d) & MASK;
                 count[c + 1]++;
             }
             // compute cumulates
-            for (int r = 0; r < R; r++)
-                count[r+1] += count[r];
+            for (int r = 0; r < R; r++) {
+                count[r + 1] += count[r];
+            }
             // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
-            if (d == w-1) {
-                int shift1 = count[R] - count[R/2];
-                int shift2 = count[R/2];
-                for (int r = 0; r < R/2; r++)
+            if (d == w - 1) {
+                int shift1 = count[R] - count[R / 2];
+                int shift2 = count[R / 2];
+                for (int r = 0; r < R / 2; r++) {
                     count[r] += shift1;
-                for (int r = R/2; r < R; r++)
+                }
+                for (int r = R / 2; r < R; r++) {
                     count[r] -= shift2;
+                }
             }
             // move data
             for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE*d) & MASK;
+                int c = (a[i] >> BITS_PER_BYTE * d) & MASK;
                 aux[count[c]++] = a[i];
             }
             // copy back
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 a[i] = aux[i];
+            }
         }
     }
 }
+/**
+ * { item_description }.
+ */
 public final class Solution {
     /**
      * Constructs the object.
