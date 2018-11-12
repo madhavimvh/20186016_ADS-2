bb60ac407b958e4fef1c994dc71bd8416540d2cb
madhavimvh <mvhmadhavi@msitprogram.net>
2018-11-12 13:36:19 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m13/Radix Sorts/Solution.java
+++ Assignments/m13/Radix Sorts/Solution.java
@@ -11,7 +11,8 @@ class LSD {
     /**
      * Constructs the object.
      */
-    public LSD() { }
+    LSD() {
+    }
    /**
      * Rearranges the array of W-character strings in ascending order.
      *
@@ -20,17 +21,17 @@ class LSD {
      */
     public static void sort(final String[] a, final int w) {
         int n = a.length;
-        int R = 256;   // extend ASCII alphabet size
+        int re = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
-
         for (int d = w - 1; d >= 0; d--) {
             // sort by key-indexed counting on dth character
             // compute frequency counts
-            int[] count = new int[R + 1];
-            for (int i = 0; i < n; i++)
+            int[] count = new int[re + 1];
+            for (int i = 0; i < n; i++) {
                 count[a[i].charAt(d) + 1]++;
+            }
             // compute cumulates
-            for (int r = 0; r < R; r++) {
+            for (int r = 0; r < re; r++) {
                 count[r + 1] += count[r];
             }
             // move data
@@ -51,37 +52,37 @@ class LSD {
      * @param      a     the array to be sorted
      */
     public static void sort(final int[] a) {
-        final int BITS = 32;
-        final int R = 1 << BITS_PER_BYTE;
-        final int MASK = R - 1;
-        final int w = BITS / BITS_PER_BYTE;
+        final int bits = 32;
+        final int re = 1 << BITS_PER_BYTE;
+        final int mask = re - 1;
+        final int w = bits / BITS_PER_BYTE;
         int n = a.length;
         int[] aux = new int[n];
-        for (int d = 0; d < w; d++) {         
+        for (int d = 0; d < w; d++) {
             // compute frequency counts
-            int[] count = new int[R + 1];
-            for (int i = 0; i < n; i++) {           
-                int c = (a[i] >> BITS_PER_BYTE * d) & MASK;
+            int[] count = new int[re + 1];
+            for (int i = 0; i < n; i++) {
+                int c = (a[i] >> BITS_PER_BYTE * d) & mask;
                 count[c + 1]++;
             }
             // compute cumulates
-            for (int r = 0; r < R; r++) {
+            for (int r = 0; r < re; r++) {
                 count[r + 1] += count[r];
             }
             // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
             if (d == w - 1) {
-                int shift1 = count[R] - count[R / 2];
-                int shift2 = count[R / 2];
-                for (int r = 0; r < R / 2; r++) {
+                int shift1 = count[re] - count[re / 2];
+                int shift2 = count[re / 2];
+                for (int r = 0; r < re / 2; r++) {
                     count[r] += shift1;
                 }
-                for (int r = R / 2; r < R; r++) {
+                for (int r = re / 2; r < re; r++) {
                     count[r] -= shift2;
                 }
             }
             // move data
             for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE * d) & MASK;
+                int c = (a[i] >> BITS_PER_BYTE * d) & mask;
                 aux[count[c]++] = a[i];
             }
             // copy back
