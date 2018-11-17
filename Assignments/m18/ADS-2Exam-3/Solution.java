98409b04bbccd71570a41b87ac56b8af336c894d
madhavimvh <mvhmadhavi@msitprogram.net>
2018-11-17 10:23:46 +0530

    "ads2_m18_a1_1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m18/ADS-2Exam-3/Solution.java
+++ Assignments/m18/ADS-2Exam-3/Solution.java
@@ -88,7 +88,17 @@ public class Solution {
 	public static BinarySearchST<String, Integer> loadDictionary(String file) {
 		BinarySearchST<String, Integer>  st = new BinarySearchST<String, Integer>();
 		// your code goes here
+		String[] words = toReadFile(file);
+		// System.out.println(words);
+		for (int i = 0; i < words.length; i++) {
+			if (!st.contains(words[i])) {
+				st.put(words[i], i);
+			} else {
+				st.put(words[i], st.get(words[i]) + 1);
+			}
+		}
 		return st;
+
 	}
 
 }
